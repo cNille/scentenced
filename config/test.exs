572043dab09ec5c1228474d5d6729edd71ad2857
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-06-30 19:23:17 +0200

    fix: docker-compose should work now

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :scentenced, Scentenced.Repo,
   username: "postgres",
   password: "postgres",
   database: "scentenced_test",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
